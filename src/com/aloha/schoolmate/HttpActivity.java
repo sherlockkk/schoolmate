8a66b6d9421088e426e535d89ffde25e34382ca9
sherlockkk <1129574214@qq.com>
2015-10-30 21:33:50 +0800

    完成主界面

--- src/com/aloha/schoolmate/HttpActivity.java
+++ src/com/aloha/schoolmate/HttpActivity.java
@@ -7,11 +7,8 @@ import android.widget.Button;
 import com.aloha.schoolmate.R.id;
 import com.lody.welike.WelikeHttp;
 import com.lody.welike.http.HttpParams;
-import com.lody.welike.http.HttpRequest;
-import com.lody.welike.http.HttpResponse;
 import com.lody.welike.http.callback.HttpResultCallback;
 import com.lody.welike.ui.WelikeActivity;
-import com.lody.welike.ui.WelikeToast;
 import com.lody.welike.ui.annotation.JoinView;
 
 public class HttpActivity extends WelikeActivity {
@@ -44,27 +41,13 @@ public class HttpActivity extends WelikeActivity {
 	// http://localhost:8888/Service/getFellowDetail?id=151091
 	private void jsonGetEvent() {
 		HttpParams params = new HttpParams();
-		params.put("id", "151091");
-		WelikeHttp.getDefault().get(
-				"http://192.168.1.114:8888/Service/getFellowDetail", params,
-				new HttpResultCallback() {
-					@Override
-					public void onSuccess(String content) {
-						super.onSuccess(content);
-						WelikeToast.toast("返回的JSON为:" + content);
-						System.out.println(content);
-					}
-
-					@Override
-					public void onFailure(HttpResponse response) {
-						super.onFailure(response);
-						WelikeToast.toast("JSON请求发送失败.");
-					}
-
-					@Override
-					public void onCancel(HttpRequest request) {
-						super.onCancel(request);
-					}
-				});
+		params.put("id", "");
+		WelikeHttp.getDefault().get("", params, new HttpResultCallback() {
+			@Override
+			public void onSuccess(String content) {
+				// TODO Auto-generated method stub
+				super.onSuccess(content);
+			}
+		});
 	}
 }
--- src/com/aloha/schoolmate/MainActivity.java
+++ src/com/aloha/schoolmate/MainActivity.java
@@ -1,41 +1,62 @@
 package com.aloha.schoolmate;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class MainActivity extends Activity {
 
-	private Button btn_query;
+	private Button btn_Query;
+	private Spinner btn_Spinner;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		btn_query = (Button) findViewById(R.id.button1);
-		btn_query.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(MainActivity.this,
-						HttpActivity.class);
-				startActivity(intent);
-
-			}
-		});
+		btn_Query = (Button) findViewById(R.id.query);
+		btn_Spinner = (Spinner) findViewById(R.id.spinner);
+		init();
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	/*
+	 * 初始化组件
+	 */
+
+	private void init() {
+		btn_Query.setOnClickListener(onClickListener);
+		btn_Spinner.setOnItemSelectedListener(onItemSelectedListener);
 	}
+
+	OnClickListener onClickListener = new OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+
+		}
+	};
+
+	OnItemSelectedListener onItemSelectedListener = new OnItemSelectedListener() {
+
+		@Override
+		public void onItemSelected(AdapterView<?> parent, View view,
+				int position, long id) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void onNothingSelected(AdapterView<?> parent) {
+			// TODO Auto-generated method stub
+
+		}
+	};
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
